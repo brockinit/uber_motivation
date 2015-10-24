6795a9a5fa197b139a627f2cbf588b373a9e35d8
Brock Lanoza <brocklanoza@gmail.com>
2015-10-23 23:43:50 -1000

    server running

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -9,13 +9,6 @@ Meteor.call('sayHello', function(err, res) {
   console.log(res);
 });
 
-if (Meteor.isServer) {
-  // Template does not support server side
-  var Template = {
-    loginButtons: 'any'
-  };
-}
-
 @reactMixin.decorate(ReactMeteorData)
 export default class App extends Component {
   getMeteorData() {
@@ -25,11 +18,15 @@ export default class App extends Component {
   }
 
   render() {
+    // Template does not suppor server side
+    let _Template = typeof(Template) === 'function' ? Template : {
+      loginButtons : 'any'
+    };
     let userCount = Users.find().fetch().length;
     let postsCount = Posts.find().fetch().length;
     return (
       <div className="App">
-        <BlazeTemplate template={Template.loginButtons} />
+        <BlazeTemplate template={_Template.loginButtons} />
         <h1>Hello Webpack!</h1>
         <p>There are {userCount} users in the Minimongo  (login to change)</p>
         <p>There are {postsCount} posts in the Minimongo  (autopublish removed)</p>
